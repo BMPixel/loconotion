4cac2b887927eb633d4043866f181b53d2e24729
BMPixel <pixelwenbo@gmail.com>
2023-04-10 11:39:17 +0000

    publish

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-python loconotion https://bmpixel.notion.site/BMPixel-27dbe9003c414c13bd5a432418911021 --chromedriver /chromedriver/chromedriver
+python loconotion https://bmpixel.notion.site/BMPixel-6f17372958844d3284524cbeea3e462c --chromedriver /chromedriver/chromedriver
 git add .
 git commit -m "publish"
 git push
