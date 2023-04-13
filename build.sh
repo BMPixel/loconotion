3a7e782cc594882b8418cb2bb613dbe13ac8debe
BMPixel <pixelwenbo@gmail.com>
2023-04-13 04:00:35 +0000

    publish

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 
 git config --global user.name "BMPixel"
 git config --global user.email "pixelwenbo@gmail.com"
-git remote set-url origin https://x-access-token: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@github.com/BMPixel/loconotion.git
+git remote set-url origin https://x-access-token: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@github.com/BMPixel/loconotion.git
 python loconotion https://bmpixel.notion.site/BMPixel-6f17372958844d3284524cbeea3e462c --chromedriver /chromedriver/chromedriver
 git add .
 git commit -m "publish"
